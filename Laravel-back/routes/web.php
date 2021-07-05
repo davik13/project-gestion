27e39548800d2c74e8d961aacffe15b031d30933
davik13 <72194058+davik13@users.noreply.github.com>
2021-07-05 14:58:18 +0200

    change project

--- Laravel-back/routes/web.php
+++ Laravel-back/routes/web.php
@@ -16,4 +16,4 @@ use App\Http\Controllers\OrganisationController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('api/organisations', OrganisationController::class);
+
